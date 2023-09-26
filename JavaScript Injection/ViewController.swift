7e51b363922700fc7b8d6e66df202264218baa80
Yuga Samuel <yugasamuel2002@gmail.com>
2023-09-26 19:10:15 +0700

    Initial Commit

---
+++ JavaScript Injection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JavaScript Injection
+//
+//  Created by Yuga Samuel on 26/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
